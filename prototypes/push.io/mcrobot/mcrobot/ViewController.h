894de645d19ca7cb8b02d27376f36b69b7f2f0b8
vitapoly inc <support@vitapoly.com>
2013-07-13 17:12:52 -0700

    initial ocmmit
    push notification working from push.io, message me for developer key.
    developer key not included

---
+++ prototypes/push.io/mcrobot/mcrobot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mcrobot
+//
+//  Created by Mark Chen on 7/13/13.
+//  Copyright (c) 2013 iosdevcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
